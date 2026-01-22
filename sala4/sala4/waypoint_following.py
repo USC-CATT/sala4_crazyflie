f10a29d837b07c80a4487dd50d135543de4fdc05
npinales <pinales@usc.edu>
2026-01-22 11:53:44 -0800

    Waypoint follower functions and discoveries about crazyflie_cpp

--- sala4/sala4/waypoint_following.py
+++ sala4/sala4/waypoint_following.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# 
+#
 # Adapted from crazyswarm2/crazyflie_examples
 
 from pathlib import Path
@@ -11,13 +11,21 @@ import rclpy
 import numpy as np
 
 
+WAYPOINTS = [
+    [[0.7, 0.1, 1.0], 0.0, 3.0],
+    [[0.5, 0.6, 0.5], 1.0, 3.0],
+    [[-0.5, 0.1, 1.0], 2.0, 3.0],
+]
+WAYPOINT_DELAY = 0.5
+
+
 def executeTrajectory(timeHelper, cf, trajpath, rate=100, offset=np.zeros(3)):
     traj = Trajectory()
     traj.loadcsv(trajpath)
-    while not timeHelper.isShutdown():
-        cf.cmdPosition(np.array([1.0,0.0,1.0]))
-        
-        timeHelper.sleepForRate(rate)
+    for i, waypoint in enumerate(WAYPOINTS):
+        print(f"Going to waypoint {i}")
+        cf.goTo(waypoint[0], waypoint[1], waypoint[2])
+        timeHelper.sleep(waypoint[2] + WAYPOINT_DELAY)
 
 
 def main():
@@ -30,7 +38,7 @@ def main():
     rate = 30.0
     Z = 0.5
     pos = np.array(cf.initialPosition) + np.array([0, 0, Z])
-    
+
     print("Attempting takeoff")
     cf.takeoff(targetHeight=Z, duration=Z+1.0)
     cf.goTo(pos, 0, 1.0)
--- scripts/prelaunch_ros.sh
+++ scripts/prelaunch_ros.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export PYTHONPATH=~crazyflie/crazyflie-firmware/build:$PYTHONPATH
+export PYTHONPATH=~/crazyflie/crazyflie-firmware/build:$PYTHONPATH
 source /opt/ros/humble/setup.bash
 source ~/crazyflie/crazyflie-ros/ros2_ws/install/setup.bash
 export GZ_SIM_RESOURCE_PATH="/home/$USER/crazyflie/crazyflie-ros/simulation_ws/crazyflie-simulation/simulator_files/gazebo/"
